c8979eb4de4def801fd0a11f02f72f137ac9c30a
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2017-09-16 19:31:37 +0300

    add level rendering

---
+++ scripts/main.js
@@ -0,0 +1,120 @@
+const arrowCodes = {
+  37: 'left',
+  38: 'up',
+  39: 'right'
+};
+
+const scale = 20;
+const maxStep = 0.05;
+const wobbleSpeed = 8;
+const wobbleDist = 0.07;
+const gravity = 30;
+const jumpSpeed = 17;
+const playerXSpeed = 7;
+
+class Level {
+  constructor(plan) {
+    this.width = plan[0].length;
+    this.height = plan.length;
+    this.grid = [];
+    this.actors = [];
+
+    for (let y = 0; y < this.height; y++) {
+      let line = plan[y], gridLine = [];
+
+      for (let x = 0; x < this.width; x++) {
+        let ch = line[x], fieldType = null;
+        let Actor = actorChars[ch];
+        if (Actor) {
+          this.actors.push(new Actor(new Vector(x, y), ch));
+        } else if (ch == 'x') {
+          fieldType = 'wall';
+        } else if (ch == '!') {
+          fieldType = 'lava';
+        }
+
+        gridLine.push(fieldType);
+      }
+      this.grid.push(gridLine);
+    }
+
+    this.player = this.actors.filter(actor => {
+      return actor.type == 'player';
+    })[0];
+    this.status = this.finishDelay = null;
+  }
+
+  isFinished() {
+    return this.status != null && this.finishDelay < 0;
+  }
+
+  obstacleAt(pos, size) {
+    let xStart = Math.floor(pos.x);
+    let xEnd = Math.ceil(pos.x + size.x);
+    let yStart = Math.floor(pos.y);
+    let yEnd = Math.ceil(pos.y + size.y);
+
+    if (xStart < 0 || xEnd > this.width || yStart < 0) {
+      return 'wall';
+    }
+
+    if (yEnd > this.height) {
+      return 'lava';
+    }
+
+    for (let y = yStart; y < yEnd; y++) {
+      for (let x = xStart; x < xEnd; x++) {
+        let fieldType = this.grid[y][x];
+        if (fieldType) {
+          return fieldType;
+        }
+      }
+    }
+  }
+
+  actorAt(actor) {
+    for (let i = 0; i < this.actors.length; i++) {
+      let other = this.actors[i];
+      if (other != actor &&
+          actor.pos.x + actor.size.x > other.pos.x &&
+          actor.pos.x < other.pos.x + other.size.x &&
+          actor.pos.y + actor.size.y > other.pos.y &&
+          actor.pos.y < other.pos.y + other.size.y) {
+
+        return other;
+      }
+    }
+  }
+
+  animate(step, keys) {
+    if (this.status != null)
+      this.finishDelay -= step;
+
+    while (step > 0) {
+      let thisStep = Math.min(step, maxStep);
+
+      // maybe doesn't have to be an arrow function?
+      this.actors.forEach(actor => {
+        actor.act(thisStep, this, keys);
+      });
+      step -= thisStep;
+    }
+  }
+
+  playerTouched(type, actor) {
+    if (type == 'lava' && this.status == null) {
+      this.status = 'lost';
+      this.finishDelay = 1;
+    } else if (type == 'coin') {
+      this.actors = this.actors.filter(other => {
+        return other != actor;
+      });
+      if (!this.actors.some(actor => {
+        return actor.type == 'coin';
+      })) {
+        this.status = 'won';
+        this.finishDelay = 1;
+      }
+    }
+  }
+}
