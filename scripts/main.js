73b087c3cd9765622ba7f83eb55546a745eea3d3
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2017-09-17 15:36:14 +0300

    add vector class

--- scripts/main.js
+++ scripts/main.js
@@ -118,3 +118,17 @@ class Level {
     }
   }
 }
+
+class Vector {
+  constructor(x, y) {
+    this.x = x; this.y = y;
+  }
+
+  plus(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+  }
+
+  times(factor) {
+    return new Vector(this.x * factor, this.y * factor);
+  }
+}
